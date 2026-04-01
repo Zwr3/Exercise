afefe2d2252664ed5f6ede4961f4d3f8f1319470
Zwr3 <2308586759@qq.com>
2026-04-01 13:43:20 +0800

    Done the Exercise9

---
+++ src/main/java/Exercise9/part1_srp/BikeService.java
@@ -0,0 +1,8 @@
+package Exercise9.part1_srp;
+
+public class BikeService {
+    public void findAvailableBikes() { System.out.println("Found bikes"); }
+    public void validateLocation() { System.out.println("Location OK"); }
+    public void reserveBike() { System.out.println("Bike reserved"); }
+    public void releaseBike() { System.out.println("Bike released"); }
+}
---
+++ src/main/java/Exercise9/part1_srp/RentalService.java
@@ -0,0 +1,7 @@
+package Exercise9.part1_srp;
+public class RentalService {
+    public void startRental() { System.out.println("Rental started"); }
+    public void endRental() { System.out.println("Rental ended"); }
+    public void cancelRental() { System.out.println("Rental canceled"); }
+    public void trackRentals() { System.out.println("Tracking rentals"); }
+}
